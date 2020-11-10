5a974dfa58d4d79574c3be077c3f1c110085e0f3
Kanyapat <kanyapat.winnerkypt@mail.kmutt.ac.th>
2020-11-10 17:28:36 +0700

    bike parcel

--- src/FastMoveConsoleApplication.java
+++ src/FastMoveConsoleApplication.java
@@ -1,13 +1,33 @@
+
+import base.Bike;
+import base.Parcel;
+import base.ParcelStatus;
+import java.util.Arrays;
+
+
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author praisan
  */
 public class FastMoveConsoleApplication {
+
+    public static Bike[] getBike() {
+        Bike[] bikes = new Bike[0];
+        bikes[0] = new Bike("eiei", 2);
+
+        return bikes;
+    }
     
+    public static Parcel[] getParcels(){
+        Parcel[] parcels = new Parcel[0];
+        parcels[0] = new Parcel(2.5, ParcelStatus.SHIPPING);
+        
+        return parcels;
+    }
 }
